6c0970521e108ddce860544515b5a80072fbc07d
arthur.jacquemin <arthurjacquemin2@gmail.com>
2022-09-21 16:21:26 +0200

    feat: store with persistance

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
 import create from "zustand";
 import { Campaign, CampaignState } from "../types";
+import { persist } from "zustand/middleware";
 
-const useCampaignStore = create<CampaignState>((set) => ({
-  campaigns: [],
-  addCampaign: (newCampaign: Campaign) =>
-    set((state: { campaigns: Campaign[] }) => ({
-      campaigns: [...state.campaigns, newCampaign],
-    })),
-}));
+const useCampaignStore = create(
+  persist<CampaignState>(
+    (set) => ({
+      campaigns: [],
+      addCampaign: (newCampaign: Campaign) =>
+        set((state: { campaigns: Campaign[] }) => ({
+          campaigns: [...state.campaigns, newCampaign],
+        })),
+    }),
+    {
+      name: "campaign-storage",
+      getStorage: () => sessionStorage,
+    }
+  )
+);
 
 export { useCampaignStore };
